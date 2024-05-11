b5b39d8af8592b6a76a179c112ac88b4cb71edcc
Abhirag <abhirag4169@gmail.com>
2024-05-11 11:05:39 +0530

    route/controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import cookieParser from 'cookieparser'
+import cookieParser from 'cookie-parser'
 
 const app=express()
 
@@ -8,17 +8,18 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))//CROSS ORIGIN REQUEST URL
-
 app.use(express.json({
     limit:"16kb"
 }))  //JSON LIMIT
-
 app.use(express.urlencoded({
     extended:true,limit:"16kb"
 }))   //TAKE FROM ANY URL
-
 app.use(express.static("public"))  //STORE TEMPORARY FILES
-
 app.use(cookieParser())  //ACCESS MODIFY COOKIES
+
+
+import userRouter from './routes/user.route.js'
+
+app.use('/api/v1/users',userRouter)
 
 export{ app }
