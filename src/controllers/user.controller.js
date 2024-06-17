2b7b4beef76d9bdd772083f1c2a5bf4693b2b1e1
Abhirag <abhirag4169@gmail.com>
2024-06-17 22:10:40 +0530

    fixed import and export errors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
+import {apiError} from "../utils/apiError.js"
 import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-import { ApiResponse } from "../utils/ApiResponse.js";
+import { apiResponse } from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
 
@@ -19,7 +19,7 @@ const generateAccessAndRefereshTokens = async(userId) =>{
 
 
     } catch (error) {
-        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+        throw new apiError(500, "Something went wrong while generating referesh and access token")
     }
 }
 
@@ -41,7 +41,7 @@ const registerUser = asyncHandler( async (req, res) => {
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
     ) {
-        throw new ApiError(400, "All fields are required")
+        throw new apiError(400, "All fields are required")
     }
 
     const existedUser = await User.findOne({
@@ -49,7 +49,7 @@ const registerUser = asyncHandler( async (req, res) => {
     })
 
     if (existedUser) {
-        throw new ApiError(409, "User with email or username already exists")
+        throw new apiError(409, "User with email or username already exists")
     }
     //console.log(req.files);
 
@@ -63,14 +63,14 @@ const registerUser = asyncHandler( async (req, res) => {
     
 
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is required")
+        throw new apiError(400, "Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if (!avatar) {
-        throw new ApiError(400, "Avatar file is required")
+        throw new apiError(400, "Avatar file is required")
     }
    
 
@@ -88,11 +88,11 @@ const registerUser = asyncHandler( async (req, res) => {
     )
 
     if (!createdUser) {
-        throw new ApiError(500, "Something went wrong while registering the user")
+        throw new apiError(500, "Something went wrong while registering the user")
     }
 
     return res.status(201).json(
-        new ApiResponse(200, createdUser, "User registered Successfully")
+        new apiResponse(200, createdUser, "User registered Successfully")
     )
 
 } )
@@ -109,7 +109,7 @@ const loginUser = asyncHandler(async (req, res) =>{
     console.log(email);
 
     if (!username && !email) {
-        throw new ApiError(400, "username or email is required")
+        throw new apiError(400, "username or email is required")
     }
     
     // Here is an alternative of above code based on logic discussed in video:
@@ -123,13 +123,13 @@ const loginUser = asyncHandler(async (req, res) =>{
     })
 
     if (!user) {
-        throw new ApiError(404, "User does not exist")
+        throw new apiError(404, "User does not exist")
     }
 
    const isPasswordValid = await user.isPasswordCorrect(password)
 
    if (!isPasswordValid) {
-    throw new ApiError(401, "Invalid user credentials")
+    throw new apiError(401, "Invalid user credentials")
     }
 
    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
@@ -146,7 +146,7 @@ const loginUser = asyncHandler(async (req, res) =>{
     .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
     .json(
-        new ApiResponse(
+        new apiResponse(
             200, 
             {
                 user: loggedInUser, accessToken, refreshToken
@@ -156,3 +156,8 @@ const loginUser = asyncHandler(async (req, res) =>{
     )
 
 })
+
+export {
+    registerUser,
+    loginUser
+}
